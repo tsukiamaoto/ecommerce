04f1d2d4d9c9276ae4b6c99c3397c5afe3687fdc
gary <gomrdhl796@gmail.com>
2019-06-10 23:05:14 +0800

    remove unnecessary color

--- src/web/store/theme.js
+++ src/web/store/theme.js
@@ -4,19 +4,15 @@ const theme = createMuiTheme({
   palette: {
     primary: {
       main: '#4fc3f7',
-      yellow: '#fff9c4',
-      orange: '#ffd54f',
-      lightGreen: '#aed581',
-      gray: '#bdbdbd',
       white: '#ffffff',
-      lightPurple: '#5c6bc0',
     },
     secondary: {
       main: '#ffffff'
     },
     pagination: '#e0e0e0',
     dollar: '#ff5722',
-    hoverItem: '#e8eaf6'
+    hoverItem: '#e8eaf6',
+    border: '#bdbdbd',
   },
 });
 
